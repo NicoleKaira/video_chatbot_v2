641b8dd73249ca1f67e91dfb804e44b1351f3668
NicoleKaira <nicoleimatong.05@gmail.com>
2025-10-31 12:23:36 +0800

    cleaned file

--- path.py
+++
@@ -1,6 +0,0 @@
-import os
-
-path = r"C:\Users\nicol\OneDrive\Desktop\Video_chatbot_v2\backend\videoindexerclient\output.json"
-print("File exists?", os.path.exists(path))
-print("Full path:", path)
-
